737dbd82572b9219950b62d087e8b4836a06ac83
Théo Hordequin <theo.hordequin@gmail.com>
2017-11-01 21:46:17 +0100

    Suggar destroy when click mouse

---
+++ The Lost Cavern/Assets/CottonCandy/Scripts/VacuumController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacuumController : MonoBehaviour {
+
+	private float distanceToObject;
+	public const int RAYCASTLENGTH = 10;
+	public CursorMode cursorMode = CursorMode.Auto;
+	public Vector2 hotSpot = new Vector2(16,16);
+	public Texture2D cursorOff, cursorDragged, cursorDraggable;
+	// Use this for initialization
+	void Start () {
+		distanceToObject = -1;
+		Cursor.SetCursor (cursorOff, hotSpot, cursorMode);
+		Cursor.visible = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hitInfo;
+		// Ray ray = GetComponentInChildren<Camera> ().ScreenPointToRay (Input.mousePosition);
+		Debug.DrawRay (this.transform.position, this.transform.forward * RAYCASTLENGTH, Color.blue);
+		bool rayCasted = Physics.Raycast (this.transform.position, this.transform.forward, out hitInfo, RAYCASTLENGTH);
+		if (rayCasted && hitInfo.transform.CompareTag ("Suggarable")) {
+			if (Input.GetMouseButtonDown (0)) {
+				Destroy (hitInfo.collider.gameObject);
+				Debug.Log ("Sugar obtenu");
+			}
+		}
+	}
+}
