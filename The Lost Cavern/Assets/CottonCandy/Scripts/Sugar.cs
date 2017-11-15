449883185ddba33265781fcddea4df717932640d
Thomas JEAN-LOUIS <thomjl78@gmail.com>
2017-11-15 21:54:21 +0100

    candy can be collected. Merge of scene done.

---
+++ The Lost Cavern/Assets/CottonCandy/Scripts/Sugar.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sugar : MonoBehaviour {
+
+    private GameObject blowerEntry;
+    private bool isDisappearing;
+
+    void Start ()
+    {
+        isDisappearing = false;
+        blowerEntry = GameObject.FindGameObjectWithTag("Blower");
+    }
+
+    void Update()
+    {        
+        if (isDisappearing)
+        {
+            //calcul distance entre le sucre et l'entre de l'aspirateur (si suffisament approche, detruit)
+
+            float distanceFromBlower = Vector3.Distance(transform.position, blowerEntry.transform.position);
+            if (distanceFromBlower <= 0.1f)
+            {
+                sugarCollected();
+            }
+
+            //quand sucre petit, scale se rapproche de 0 automatiquement
+            transform.localScale -= (transform.localScale * 99/100) * Time.deltaTime;
+
+            Vector3 dir = blowerEntry.transform.position - transform.position;
+            transform.Translate(dir.normalized * Time.deltaTime * 0.5f , Space.World);
+            //transform.position = Vector3.MoveTowards(transform.position, vacuumEntry.transform.position , 2*Time.deltaTime);
+
+            if (transform.localScale.x <= 0.01f)
+            {
+                
+                sugarCollected();
+            }
+        }
+        else if (transform.localScale.x <= 0.05f)
+        {
+            isDisappearing = true;
+
+            GetComponent<Collider>().isTrigger = true;//pour eviter les collisions avec l'aspirateur
+            //Destroy(gameObject);
+        }
+               
+    }
+
+    void sugarCollected()
+    {
+        Destroy(gameObject);
+    }
+}
--- The Lost Cavern/Assets/CottonCandy/Scripts/VacuumController.cs
+++ The Lost Cavern/Assets/CottonCandy/Scripts/VacuumController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VacuumController : MonoBehaviour {
+       
+   // public float aspirationSpeed;
 
 	private float distanceToObj;
 	private int RAYCASTLENGTH = 5;
@@ -11,32 +13,71 @@ public class VacuumController : MonoBehaviour {
 	public CursorMode cursorMode = CursorMode.Auto;
 	public Vector2 hotSpot = new Vector2(16, 16);	// Offset du centre du curseur
 
-	public int sugarInventory = 0;
-	// Use this for initialization
+	public int sugarInventory = 0;	
+
+    private bool canBlowSugar;
+    private GameObject sugarTarget;
+
 	void Start () 
 	{
 		distanceToObj = -1;	
 		Cursor.visible = true;
+        canBlowSugar = false;
 	}
 	
 	void Update () {
-		AspiratorBlower ();
+		//AspiratorBlower ();
+        if (sugarTarget != null)
+        {
+            if (canBlowSugar && Input.GetMouseButtonDown(0))
+            {
+                sugarTarget.transform.localScale -= (sugarTarget.transform.localScale * 80/100) * Time.deltaTime;
+                //sugarTarget.GetComponent<Sugar>().vacuumEntry = gameObject;
+            }
+
+            if (canBlowSugar && Input.GetMouseButton(0))
+            {
+                sugarTarget.transform.localScale -= (sugarTarget.transform.localScale * 80/100) * Time.deltaTime;
+                //sugarTarget.GetComponent<Sugar>().vacuumEntry = gameObject;
+            }
+        }
+
+
 	}
 
 	void AspiratorBlower () {
+
+        /*
 		RaycastHit hitInfo;
-		Ray ray = GetComponentInChildren<Camera>().ScreenPointToRay(Input.mousePosition);
+       // Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        Ray ray = new Ray(transform.position, transform.forward);
+
 		Debug.DrawRay (ray.origin, ray.direction * RAYCASTLENGTH, Color.blue);
 		bool rayCasted = Physics.Raycast (ray, out hitInfo, RAYCASTLENGTH);
 		if (Input.GetMouseButton (0)) {
 			if (rayCasted && hitInfo.transform.CompareTag ("Suggarable")) {
-				Destroy (hitInfo.collider.gameObject);
-				SugarGenerator.sugarCounter--;
-				ManageSugar ();
+                
+				//Destroy (hitInfo.collider.gameObject);
+				//SugarGenerator.sugarCounter--;
+				//ManageSugar ();
+
+                //diminue la taille du sucre
+                GameObject sugar = hitInfo.transform.gameObject;
+
+                //reduction de la taille du sucre
+                sugar.transform.localScale -= (sugar.transform.localScale * 80/100) * Time.deltaTime;
+                sugar.GetComponent<Sugar>().vacuumEntry = gameObject;
+
+                //sucre se rapproche de l'aspirateur
+               // Vector3 dir = transform.position - sugar.transform.position;
+                //sugar.transform.Translate(dir.normalized * Time.deltaTime * aspirationSpeed, Space.World);
+
+
 			} else if (rayCasted && hitInfo.transform.CompareTag ("CandyMachine")) {
 				Debug.Log("Ce n'est pas encore le moment, récupère des sucres!");
 			}
 		}
+  */      
 	}
 		
 	void ManageSugar () {
@@ -46,4 +87,25 @@ public class VacuumController : MonoBehaviour {
 	void displayNumberSugar () {
 
 	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Suggarable")
+        {
+            Debug.Log("sugar detected");
+
+            sugarTarget = other.gameObject;
+            //diminue la taille du sucre
+            canBlowSugar = true;
+           
+        }
+    }
+
+    void OnTriggerExit ()
+    {
+        if (canBlowSugar)
+        {
+            canBlowSugar = false;
+        }
+    }
 }
