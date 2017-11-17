47d1d6fbe20659fb04c5a20bb90bea8a2d108e00
Théo Hordequin <theo.hordequin@gmail.com>
2017-11-17 15:48:27 +0100

    Add UI and rigth rules for the game

---
+++ The Lost Cavern/Assets/CottonCandy/Scripts/ScoreDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+
+	Text text;
+
+	void Start()
+	{
+		text = GetComponent<Text>();
+	}
+
+	void Update()
+	{
+		text.text = "Il vous manque " + GameFunctions.goal + " sucre(s)";
+		if (GameFunctions.goal == 0)
+		{
+			text.text = "Vous avez collectés assez de sucres, versez les dans la machine";
+		}
+	}
+}
--- The Lost Cavern/Assets/CottonCandy/Scripts/SugarGenerator.cs
+++ The Lost Cavern/Assets/CottonCandy/Scripts/SugarGenerator.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class SugarGenerator : MonoBehaviour {
 
-	const int NUMBEROFSUGARS = 20;
+	const int NUMBEROFSUGARS = 5;
 	public static int sugarCounter;
 	public GameObject plane;
 	public GameObject sugar;
 	Bounds planeBounds;
 	public Vector3 sugarSize;
-
+	public static bool isFinished = false;
+	public GameObject[] gameObjects;
+	public GameObject blower;
 
 	// Use this for initialization
 	void Start () {
 		sugarCounter = 0;
 		planeBounds = plane.GetComponent<Renderer> ().bounds;
-		sugarSize = sugar.GetComponent<Renderer> ().bounds.size;
+		sugarSize = sugar.GetComponent<Renderer>().bounds.size;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (sugarCounter < NUMBEROFSUGARS) {
+		if (sugarCounter < NUMBEROFSUGARS && !isFinished) {
 			float x = Random.Range (planeBounds.min.x + 1, planeBounds.max.x);
 			float y = Random.Range (1f, 2f);
 			float z = Random.Range (planeBounds.min.z + 1, planeBounds.max.z -1);
@@ -30,9 +32,25 @@ public class SugarGenerator : MonoBehaviour {
 			Instantiate(sugar, sugarPosition, Quaternion.identity);
 			sugarCounter++;
 		}
+		if (isFinished)
+		{
+			destroyAllSugar();
+		}
 	}
 
 	public void getSugar() {
 		sugarCounter--;
+	}
+
+	void destroyAllSugar()
+	{
+		gameObjects = GameObject.FindGameObjectsWithTag("Suggarable");
+
+		for (var i = 0; i < gameObjects.Length; i++)
+		{
+			Destroy(gameObjects[i]);
+		}
+		blower.SetActive(false);
+
 	}
 }
